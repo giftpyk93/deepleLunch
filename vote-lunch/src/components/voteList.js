470108fb074e19ca549126b2b328e3e53d6297ef
Piyakorn Jansiripitikul <giftzchan@gmail.com>
2019-07-07 13:51:12 +0700

    add check component to votelist

--- vote-lunch/src/components/voteList.js
+++ vote-lunch/src/components/voteList.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const VoteList = () => {
-  return <div>
-    list
-  </div>
+import Checkbox from './check'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex: 1;
+  padding: 1rem;
+  width: 92%;
+  flex-wrap: wrap;
+`
+
+const VoteList = props => {
+  const { list, handleVoteChange, handleAddOption } = props
+  return <Container>
+    {list && list.map(data => <Checkbox text={data.text} initCheck={data.checked} handleChangeChecked={handleVoteChange} />)}
+    <Checkbox text="Add an option" isAddOption addOptionClick={handleAddOption} />
+  </Container>
 }
 
 export default VoteList
