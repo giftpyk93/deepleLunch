36e69c75ada9f0489c79260c27b53f75b9eb12f7
Piyakorn Jansiripitikul <giftzchan@gmail.com>
2019-07-07 13:50:43 +0700

    add check component

---
+++ vote-lunch/src/components/check.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 43%;
+  padding: 1rem;
+`
+
+const Checkbox = styled.input`
+  width: 2rem;
+  height: 2rem;
+  margin-right: 1.2rem;
+`
+
+const Label = styled.span`
+  display: flex;
+  flex: 1;
+  font-size: 1.7rem;
+  ${props => props.fade && `
+    color: #CCC;
+  `}
+`
+
+const Check = props => {
+  const { text, handleChangeChecked, initCheck, isAddOption, addOptionClick } = props
+  const [checkedValue, setCheckedValue] = useState(initCheck)
+  const onClickChecked = () => {
+    // handleChangeChecked(!checkedValue)
+    setCheckedValue(!checkedValue)
+  }
+
+  return <Container onClick={() => isAddOption && addOptionClick()}>
+    <Checkbox type="checkbox" checked={!isAddOption && checkedValue} onClick={onClickChecked} disabled={isAddOption} />
+    <Label fade={isAddOption}>{text}</Label>
+  </Container>
+}
+
+export default Check
