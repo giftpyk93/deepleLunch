bba4d571400e6d32877de90b3d997386c4821551
Piyakorn Jansiripitikul <giftzchan@gmail.com>
2019-07-09 11:24:19 +0700

    fix header

--- vote-lunch/src/components/haeder.js
+++ vote-lunch/src/components/haeder.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { COLORS } from '../styled'
+import logoutLogo from '../logout.png'
 
 const Container = styled.div`
   display: flex;
@@ -12,13 +13,38 @@ const Container = styled.div`
   font-weight: bold;
 `
 
+const LogoutIcon = styled.div`
+  min-width: 2rem;
+  height: 2rem;
+  background-position: center;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-image: url(${logoutLogo});
+  margin-left: 1rem;
+`
+
+const Info = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+  width: 35%;
+`
+
+const Ellipsis = styled.span`
+  text-overflow: ellipsis;
+  min-width: 0;
+  white-space: nowrap;
+  overflow: hidden;
+  font-size: 1.5rem;
+  font-weight: normal;
+`
+
 const Header = props => {
   const { user, title, handleLogout } = props
   return <Container>
   <span>{title}</span>
-  <div>
-    {user && <div>{user}<div onClick={handleLogout}>Logout</div></div>}
-  </div>
+  {user && <Info><Ellipsis>{user}</Ellipsis><LogoutIcon onClick={handleLogout} /></Info>}
   </Container>
 }
 
